cfef88c44e53409e23e40f39853dd9ea3f1e2759
Brendon <brendon.lopes.21@gmail.com>
2022-12-27 23:56:18 -0300

    feat: :sparkles: add a NavigatorComponent

---
+++ client/src/components/NavigatorComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <navigator>
+    <img src="../assets/logo.svg" class="logo" alt="cashforce logo" />
+
+    <section>
+      <div class="navigator-item">
+        <div class="highlight-bar" />
+        <img
+          src="../assets/iconHands.svg"
+          class="hands-icon"
+          alt="ícone aperto de mãos"
+        />
+        <p class="navigator-item-text">Notas fiscais</p>
+      </div>
+    </section>
+  </navigator>
+</template>
+
+<style scoped>
+navigator {
+  display: flex;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fbfdfe;
+  flex-direction: column;
+}
+.logo {
+  margin: 25px 24px;
+  width: 160px;
+  height: 30px;
+}
+.navigator-item {
+  display: flex;
+  align-items: center;
+}
+.navigator-item:hover {
+  cursor: pointer;
+  background-color: #ececec;
+}
+.highlight-bar {
+  width: 4px;
+  height: 64px;
+  background-color: var(--base-green);
+  border-radius: 4px;
+}
+.hands-icon {
+  width: 24px;
+  margin-left: 24px;
+}
+.navigator-item-text {
+  font-weight: 500;
+  color: var(--base-green);
+  margin-left: 15px;
+  font-size: 14px;
+  line-height: 18.23px;
+}
+</style>
