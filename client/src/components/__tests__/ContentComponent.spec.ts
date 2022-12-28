2c8a4eb6829be4152a8fc0ca9c1f855586e049b4
Brendon <brendon.lopes.21@gmail.com>
2022-12-28 18:25:31 -0300

    test: :white_check_mark: add tests to Content component

---
+++ client/src/components/__tests__/ContentComponent.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import ContentComponent from "@/components/ContentComponent.vue";
+
+describe("Content Component", () => {
+  it("renders properly", () => {
+    const wrapper = mount(ContentComponent);
+
+    expect(wrapper.text()).toContain("Notas fiscais");
+
+    expect(wrapper.text()).toContain(
+      "Visualize as notas fiscais que você tem."
+    );
+
+    expect(wrapper.find("img").attributes("src")).toBe(
+      "/assets/iconHandsBlue.svg"
+    );
+
+    expect(wrapper.findComponent("Table").exists()).toBe(true);
+  });
+});
