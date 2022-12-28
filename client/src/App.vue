549ae6e3a5b8478e9535521f73d31474f297b6cd
Brendon <brendon.lopes.21@gmail.com>
2022-12-28 16:28:30 -0300

    feat: :sparkles: add content component to app
    also add a wrapper with the "app" class to make it easier to change CSS properties

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import Navigator from "./components/NavigatorComponent.vue";
+import Content from "./components/ContentComponent.vue";
 </script>
 
 <template>
-  <Navigator />
+  <div class="app">
+    <Navigator />
+    <Content />
+  </div>
 </template>
+
+<style scoped>
+.app {
+  display: flex;
+  width: 100%;
+}
+</style>
