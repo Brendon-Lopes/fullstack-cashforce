656f5db1107b45e377747d34cad2a42e06eb1632
Brendon <brendon.lopes.21@gmail.com>
2022-12-30 18:18:01 -0300

    feat: :card_file_box: add OrderPortion model
    for 'orderportions' table

---
+++ server/src/database/models/OrderPortion.ts
@@ -0,0 +1,34 @@
+import { BelongsTo, Column, CreatedAt, Default, ForeignKey, Model, Table, UpdatedAt } from 'sequelize-typescript'
+import { Order } from './Order'
+
+@Table({ tableName: 'orderportions' })
+export class OrderPortion extends Model<OrderPortion> {
+  @Column
+    nDup!: string
+
+  @Column
+    dVenc!: string
+
+  @Column
+    vDup!: string
+
+  @Default(1)
+  @Column
+    availableToMarket!: number
+
+  @CreatedAt
+  @Column
+    createdAt!: Date
+
+  @UpdatedAt
+  @Column
+    updatedAt!: Date
+
+  @ForeignKey(() => Order)
+  @Default(null)
+  @Column
+    orderId?: number
+
+  @BelongsTo(() => Order)
+    order?: Order
+}
