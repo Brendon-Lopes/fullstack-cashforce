97e1bd506208cf5189a84078860cd6d79711ddcf
Brendon <brendon.lopes.21@gmail.com>
2022-12-30 00:37:16 -0300

    feat: :card_file_box: add sequelize configuration file

---
+++ server/src/database/config.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import 'dotenv/config'
+import { Sequelize } from 'sequelize-typescript'
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: 'mysql',
+  dialectOptions: {
+    multipleStatements: true
+  },
+  models: [path.join(__dirname, '/models')]
+})
+
+export default sequelize
