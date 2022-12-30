eae4fca897ec7dac37d9c3df6c14c90aaae77a27
Brendon <brendon.lopes.21@gmail.com>
2022-12-30 18:19:12 -0300

    feat: :card_file_box: add User model
    for 'users' table

---
+++ server/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { Column, CreatedAt, Default, Model, Table, UpdatedAt } from 'sequelize-typescript'
+
+@Table({ tableName: 'users' })
+export class User extends Model<User> {
+  @Column
+    name!: string
+
+  @Column
+    email!: string
+
+  @Default(null)
+  @Column
+    phonenumber?: string
+
+  @Default(null)
+  @Column
+    mobile?: string
+
+  @Default(null)
+  @Column
+    departament?: string
+
+  @Default(null)
+  @Column
+    verificationcode?: string
+
+  @Default(0)
+  @Column
+    emailchecked?: number
+
+  @Default(0)
+  @Column
+    cashforceadm?: number
+
+  @CreatedAt
+  @Column
+    createdat!: Date
+
+  @UpdatedAt
+  @Column
+    updatedat!: Date
+}
