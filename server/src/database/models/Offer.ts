7fb974940c06bb0f9f34d89b8b131d121e302f1f
Brendon <brendon.lopes.21@gmail.com>
2022-12-30 18:17:29 -0300

    feat: :card_file_box: add Offer model
    for 'offers' table

---
+++ server/src/database/models/Offer.ts
@@ -0,0 +1,56 @@
+import { BelongsTo, Column, CreatedAt, Default, ForeignKey, Model, Table, UpdatedAt } from 'sequelize-typescript'
+import { Order } from './Order'
+import { Sponsor } from './Sponsor'
+
+@Table({ tableName: 'offers' })
+export class Offer extends Model<Offer> {
+  @Column
+    tax!: string
+
+  @Column
+    tariff!: string
+
+  @Column
+    adValorem!: string
+
+  @Column
+    float!: string
+
+  @Column
+    iof!: string
+
+  @Column
+    expiresIn!: Date
+
+  @Default(0)
+  @Column
+    paymentStatusSponsor!: number
+
+  @Default(0)
+  @Column
+    paymentStatusProvider!: number
+
+  @CreatedAt
+  @Column
+    createdAt!: Date
+
+  @UpdatedAt
+  @Column
+    updatedAt!: Date
+
+  @ForeignKey(() => Order)
+  @Default(null)
+  @Column
+    orderId?: number
+
+  @BelongsTo(() => Order)
+    order?: Order
+
+  @ForeignKey(() => Sponsor)
+  @Default(null)
+  @Column
+    sponsorId?: number
+
+  @BelongsTo(() => Sponsor)
+    sponsor?: Sponsor
+}
