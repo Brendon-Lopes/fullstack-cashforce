2bfe42e4375b5ec098fda74f432c375c6d57956d
Brendon <brendon.lopes.21@gmail.com>
2022-12-31 16:34:05 -0300

    test: :white_check_mark: add tests to GetAllOrdersController class

---
+++ server/src/controllers/GetAllOrdersController.spec.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import sinon from 'sinon'
+import chai from 'chai'
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { Buyer } from '../database/models/Buyer'
+import { Order } from '../database/models/Order'
+import { Provider } from '../database/models/Provider'
+import { GetAllOrdersRepository } from '../repositories/GetAllOrdersRepository'
+import { GetAllOrdersUseCase } from '../useCases/GetAllOrdersUseCase'
+import { testDatabase } from '../utils/testDatabase'
+import { GetAllOrdersController } from './GetAllOrdersController'
+import { ordersTestSeeder } from '../utils/testSeeders/ordersTestSeeder'
+
+const { expect } = chai
+
+describe('GetAllOrdersController class', () => {
+  const getAllOrdersRepository = new GetAllOrdersRepository(
+    testDatabase.getRepository(Order),
+    testDatabase.getRepository(Buyer),
+    testDatabase.getRepository(Provider)
+  )
+  const getAllOrdersUseCase = new GetAllOrdersUseCase(getAllOrdersRepository)
+  const getAllOrdersController = new GetAllOrdersController(getAllOrdersUseCase)
+
+  const req = {} as Request
+  const res = {} as Response
+
+  before(async () => {
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub()
+    await testDatabase.sync({ force: true })
+    await testDatabase.getRepository(Order).bulkCreate(ordersTestSeeder as unknown as Order[])
+  })
+
+  it('should return status code 200 (OK)', async () => {
+    await getAllOrdersController.execute(req, res)
+
+    expect((res.status as sinon.SinonStub).calledOnce).to.be.true
+    expect((res.status as sinon.SinonStub).calledWith(StatusCodes.OK)).to.be.true
+
+    expect((res.json as sinon.SinonStub).calledOnce).to.be.true
+  })
+})
