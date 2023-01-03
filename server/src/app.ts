181a3c8b4b1ad8f55beb14b7b499a8fd57c377d0
Brendon <brendon.lopes.21@gmail.com>
2023-01-03 20:40:19 -0300

    docs: :memo: add docs route '/api-docs'

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,15 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import router from './routes'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from './docs/swagger.json'
 
 export const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.get('/', (req, res) => res.status(418).send())
 
