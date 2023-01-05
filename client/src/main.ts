e80b98d1c54fc0fb84526f4ba1d234cf7a824f6b
Brendon <brendon.lopes.21@gmail.com>
2023-01-05 11:29:09 -0300

    feat: :wrench: add vue-toastification configs to app

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
+import Toast, { type PluginOptions, POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+const options: PluginOptions = {
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 5,
+  newestOnTop: true,
+  position: POSITION.BOTTOM_RIGHT,
+  pauseOnHover: false,
+  pauseOnFocusLoss: false,
+};
+
+createApp(App).use(Toast, options).mount("#app");
